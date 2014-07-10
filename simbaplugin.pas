e3eaad82305b708b8ce140dd4f6860d13049d599
John Peel <john@dgby.org>
2014-07-10 05:52:23 -0400

    Added copywrite headers to source files

--- simbaplugin.pas
+++ simbaplugin.pas
@@ -1,3 +1,23 @@
+{
+    libThread - A threading plugin for Simba
+    Copyright (C) 2014 by John Peel
+
+    libThread is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    libThread is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+    See the file LICENSE, included in this distribution,
+    for details about the copyright.
+}
 unit SimbaPlugin;
 
 {$mode objfpc}{$H+}
